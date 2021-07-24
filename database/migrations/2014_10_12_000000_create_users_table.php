901a3d15413366cb29ba69005114d9bade1f4f89
Mohammad Javad Ghasemy <geeksesi@gmail.com>
2021-07-24 15:40:13 +0430

    some fixes on user

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -13,12 +13,17 @@ class CreateUsersTable extends Migration
      */
     public function up()
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create("users", function (Blueprint $table) {
             $table->id();
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
+            $table->string("name")->nullable();
+            $table->string("phone")->unique();
+            $table->string("password")->nullable();
+            $table
+                ->string("email")
+                ->unique()
+                ->nullable();
+            // bitwise operation
+            $table->integer("role")->default(1);
             $table->rememberToken();
             $table->timestamps();
         });
@@ -31,6 +36,6 @@ class CreateUsersTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists("users");
     }
 }
