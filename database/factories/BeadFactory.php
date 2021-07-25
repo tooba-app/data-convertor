0ab5c46365f59d7b7dbca911000e1588acc358d0
Mohammad Javad Ghasemy <geeksesi@gmail.com>
2021-07-25 19:23:33 +0430

    some structure for tooba

---
+++ database/factories/BeadFactory.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\Bead;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+class BeadFactory extends Factory
+{
+    /**
+     * The name of the factory's corresponding model.
+     *
+     * @var string
+     */
+    protected $model = Bead::class;
+
+    /**
+     * Define the model's default state.
+     *
+     * @return array
+     */
+    public function definition()
+    {
+        return [
+            //
+        ];
+    }
+}
