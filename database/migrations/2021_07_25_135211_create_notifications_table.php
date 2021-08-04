bc84348027ef4aa0154da6cbc95f120fdb04d94c
Mohammad Javad Ghasemy <geeksesi@gmail.com>
2021-08-04 15:53:31 +0430

    make structure of notifications. but i'm not sure about it

--- database/migrations/2021_07_25_135211_create_notifications_table.php
+++ database/migrations/2021_07_25_135211_create_notifications_table.php
@@ -13,9 +13,39 @@ class CreateNotificationsTable extends Migration
      */
     public function up()
     {
-        Schema::create('notifications', function (Blueprint $table) {
+        Schema::create("notifications", function (Blueprint $table) {
             $table->id();
+            $table
+                ->foreignId("creator_id")
+                ->constrained("users")
+                ->onDelete("SET NULL");
+            $table
+                ->foreignId("action_id")
+                ->constrained("action")
+                ->onDelete("CASCADE");
+
+            $table
+                ->foreignId("title_id")
+                ->constrained("texts", "text_id")
+                ->onDelete("SET NULL");
+            $table
+                ->foreignId("description_id")
+                ->constrained("texts", "text_id")
+                ->onDelete("SET NULL");
+
+            $table
+                ->foreignId("date_id")
+                ->constrained("tooba_dates", "id")
+                ->onDelete("CASCADE")
+                ->nullable();
+            $table
+                ->foreignId("time_id")
+                ->constrained("tooba_times", "id")
+                ->onDelete("CASCADE")
+                ->nullable();
+
             $table->timestamps();
+            $table->softDeletes();
         });
     }
 
@@ -26,6 +56,6 @@ class CreateNotificationsTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('notifications');
+        Schema::dropIfExists("notifications");
     }
 }
