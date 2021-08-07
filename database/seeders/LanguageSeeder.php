32afdc2979cdc138c45835d37678867590d0fc11
Mohammad Javad Ghasemy <geeksesi@gmail.com>
2021-08-07 14:50:32 +0430

    make some default data for language

--- database/seeders/LanguageSeeder.php
+++ database/seeders/LanguageSeeder.php
@@ -2,7 +2,9 @@
 
 namespace Database\Seeders;
 
+use App\Models\Language;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
 
 class LanguageSeeder extends Seeder
 {
@@ -13,6 +15,32 @@ class LanguageSeeder extends Seeder
      */
     public function run()
     {
-        //
+        Language::create([
+            "code" => "ar",
+            "name" => "arabic",
+            "localname" => "العربية",
+            "direction" => "rtl",
+        ]);
+
+        Language::create([
+            "code" => "ar_IQ",
+            "name" => "arabic",
+            "localname" => "العربية",
+            "direction" => "rtl",
+        ]);
+
+        Language::create([
+            "code" => "en_US",
+            "name" => "english",
+            "localname" => "english",
+            "direction" => "rtl",
+        ]);
+
+        Language::create([
+            "code" => "fa_IR",
+            "name" => "persian",
+            "localname" => "فارسی",
+            "direction" => "rtl",
+        ]);
     }
 }
